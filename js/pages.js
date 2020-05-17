ef1fcfaf74a7e697ceee814d7b80d4ac9fc329ce
Mário Silva <47184969+MarioCSilva@users.noreply.github.com>
2020-05-18 00:50:02 +0100

    some js

--- js/pages.js
+++ js/pages.js
@@ -1,6 +1,34 @@
     
 $(document).ready(function () {
-  console.log("ola boi")
+  var users = JSON.parse(localStorage.getItem("users"));
+  var user= users["currentUser"];
+  var pages = user.pages
+
+  $.each(pages, function (index, value) {
+    $("#grid").append(`
+    <div class="card  bg-dark text-white " style="margin-bottom:15px; ">
+    <div class="card-header">`+value.split("//")[0].split("/")[0]+`</div>
+    <div class="card-body bg-light  text-dark">`+value.split("//")[0].split("/")[1]+`</div>
+    </div>
+    `);
+  });
+
+  $("#addTask").click(function (event) {
+    if ($("#name").val()=="" || $("#desc").val()==""){
+      alert("Please fill name and description of the page");
+    } else{
+      var res=$("#name").val()+"/"+$("#desc").val()+"/"+$("#col").val();
+      console.log(res);
+      users["currentUser"].pages.push(res);
+      users[users["currentUser"].username].pages.push(res);
+      localStorage.setItem("users", JSON.stringify(users));
+      location.reload();
+    }
+  });
+
+
+
+
     var modal = document.getElementById("myModal");
 
     // Get the button that opens the modal
