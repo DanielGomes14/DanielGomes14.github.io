c98d53a6c3ece5cac5f610df178daba5f4075336
DanielGomes14 <48451636+DanielGomes14@users.noreply.github.com>
2020-05-17 22:48:26 +0100

    bro leandro para de copiar

---
+++ js/personalpage.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+var users = JSON.parse(localStorage.getItem("users"));
+var user= users["currentUser"];
+var pages = user.pages
+ $.each(rm, function (index, value) {
+
+ })
+});	
